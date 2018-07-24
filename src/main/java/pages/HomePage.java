f52162175d359d5d5f39a0d9438052e24d76d650
ivan2007000 <41183245+ivan2007000@users.noreply.github.com>
2018-07-24 11:08:14 +0300

    Update HomePage.java

--- src/main/java/pages/HomePage.java
+++ src/main/java/pages/HomePage.java
@@ -13,42 +13,42 @@ public class HomePage {
 
 	
 	WebDriver driver;
-	//описание элементов на странице
+	//РѕРїРёСЃР°РЅРёРµ СЌР»РµРјРµРЅС‚РѕРІ РЅР° СЃС‚СЂР°РЅРёС†Рµ
 	By homePageName = By.id("PH_user-email");
-	By letterbutton = By.xpath("//span[contains(text(), 'Написать письмо')]");
+	By letterbutton = By.xpath("//span[contains(text(), 'РќР°РїРёСЃР°С‚СЊ РїРёСЃСЊРјРѕ')]");
 	By contact = By.xpath("//div[@data-blockid='compose_to']/div");
 	By theme = By.xpath("//input[@name='Subject']");
 	By lettertext = By.xpath("//body[@contenteditable='true']");
-	By lettersend = By.xpath("//span[contains(text(), 'Отправить')]");
+	By lettersend = By.xpath("//span[contains(text(), 'РћС‚РїСЂР°РІРёС‚СЊ')]");
 	By lettersuccess = By.xpath("//div[@class='message-sent__title']");
 	
 	public HomePage(WebDriver driver){
 		this.driver = driver;
 	}
 	
-	// логин почты на странице
+	// Р»РѕРіРёРЅ РїРѕС‡С‚С‹ РЅР° СЃС‚СЂР°РЅРёС†Рµ
 		public String getHomePageDashboardName(){
 			new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(homePageName));
 		 return	driver.findElement(homePageName).getText();
 		}
-		// Заполнение поля "Кому"
+		// Р—Р°РїРѕР»РЅРµРЅРёРµ РїРѕР»СЏ "РљРѕРјСѓ"
 		public void setcontact(String strcontact) throws Exception{
 			Actions builder = new Actions(driver);
 			WebElement element = driver.findElement(contact);
 			builder.moveToElement(element).click().sendKeys(strcontact).perform();
 			}
-		//Заполнение поля "Тема сообщения"
+		//Р—Р°РїРѕР»РЅРµРЅРёРµ РїРѕР»СЏ "РўРµРјР° СЃРѕРѕР±С‰РµРЅРёСЏ"
 		public void settheme(String strtheme) throws Exception{
 			 
 			driver.findElement(theme).sendKeys(strtheme);}
-		//Заполнение "тела" письма
+		//Р—Р°РїРѕР»РЅРµРЅРёРµ "С‚РµР»Р°" РїРёСЃСЊРјР°
 		public void setlettertext(String strlettertext) throws Exception{
 			new WebDriverWait(driver, 20).until(ExpectedConditions.visibilityOfElementLocated(lettertext));
 			driver.findElement(lettertext).sendKeys(strlettertext);}
-		// Кнопки на странице
+		// РљРЅРѕРїРєРё РЅР° СЃС‚СЂР°РЅРёС†Рµ
 		public void clicknewletter(){driver.findElement(letterbutton).click();}
 		public void sendletter(){driver.findElement(lettersend).click();}
-		// метод создания и отправки письма
+		// РјРµС‚РѕРґ СЃРѕР·РґР°РЅРёСЏ Рё РѕС‚РїСЂР°РІРєРё РїРёСЃСЊРјР°
 		public void createNewLetter(String strcontact, String strtheme, String strlettertext ) throws Exception{
 			clicknewletter();
 			this.setcontact(strcontact);
@@ -62,7 +62,7 @@ public class HomePage {
 			driver.switchTo().defaultContent();
 			sendletter();
 			}
-		//Проверка статуса отправки письма
+		//РџСЂРѕРІРµСЂРєР° СЃС‚Р°С‚СѓСЃР° РѕС‚РїСЂР°РІРєРё РїРёСЃСЊРјР°
 		public String getsuccesssendstatus(){
 			 return	driver.findElement(lettersuccess).getText();
 			}
